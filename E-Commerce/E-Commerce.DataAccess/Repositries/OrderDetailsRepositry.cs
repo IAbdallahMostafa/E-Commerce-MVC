f6e6f2cfd7d19de6c3ad2e9ef846c4a76bfc92f8
Abdallah Mostafa <abdallah.mostafa.draz@gmail.com>
2025-03-13 14:42:39 +0200

    Create OrderHeader and OrderDetails Repositries

---
+++ E-Commerce/E-Commerce.DataAccess/Repositries/OrderDetailsRepositry.cs
@@ -0,0 +1,11 @@
+﻿using E_Commerce.DataAccess.Data;
+using E_Commerce.Entites.Intefaces;
+using E_Commerce.Entites.Models;
+
+namespace E_Commerce.DataAccess.Repositries
+{
+    public class OrderDetailsRepositry : GenericRepositry<OrderDetails>, IOrderDetailsRepositry
+    {
+        public OrderDetailsRepositry(AppDBContext context) : base(context) { }
+    }
+}
---
+++ E-Commerce/E-Commerce.DataAccess/Repositries/OrderHeaderRepositry.cs
@@ -0,0 +1,11 @@
+﻿using E_Commerce.DataAccess.Data;
+using E_Commerce.Entites.Intefaces;
+using E_Commerce.Entites.Models;
+
+namespace E_Commerce.DataAccess.Repositries
+{
+    public class OrderHeaderRepositry : GenericRepositry<OrderHeader>, IOrderHeaderRepositry
+    {
+        public OrderHeaderRepositry(AppDBContext context) : base(context) { }
+    }
+}
